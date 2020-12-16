b611dc28b34d501b1f62b8f410b1620bea5ffc05
Sidoupiar <sidoupiar@qq.com>
2020-12-16 13:58:43 +0800

    优化代码

--- startup/generator/sigen_entity_health_unit.lua
+++ startup/generator/sigen_entity_health_unit.lua
@@ -3,6 +3,12 @@ entity:AddDefaultValue( "defaultType" , SITypes.entity.unit )
 
 
 
+function entity:DefaultFlags()
+	return self:SetParam( "flags" , { SIFlags.entityFlags.placeablePlayer , SIFlags.entityFlags.placeableEnemy , SIFlags.entityFlags.placeableOffGrid , SIFlags.entityFlags.breathsAir , SIFlags.entityFlags.notRepairable } )
+end
+
+
+
 function entity:Init( currentEntity )
 	if not currentEntity then currentEntity = self end
 	self.super:Init( currentEntity )
