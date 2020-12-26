7993383d39075b391937972392510900404d27f1
Sidoupiar <sidoupiar@qq.com>
2020-12-26 16:51:19 +0800

    版本更新

--- migrations/2020.12.26_SICoreFunctionLibrary_0.4.3.lua
+++ migrations/2020.12.26_SICoreFunctionLibrary_0.4.3.lua
@@ -5,6 +5,19 @@
 -- 创建全局引用
 SIGlobal.CreateOnMigrations()
 
+local players = {}
+for playerIndex , viewData in pairs( SITitlebarViews ) do
+	SITitlebar.CloseView( playerIndex , viewData )
+	table.insert( players , playerIndex )
+end
+
 for playerIndex , viewData in pairs( SIViewGameSpeedViews ) do
 	if not viewData.data then viewData.data = {} end
 end
+
+for index , playerIndex in pairs( players ) do
+	local event = { player_index = playerIndex }
+	SITitlebar.OnInitPlayer( event )
+end
+
+SIOremap.HideViews()
