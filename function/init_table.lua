9a4f601a1dcd6ab512559e3bb502f74a5497b084
Sidoupiar <sidoupiar@qq.com>
2021-03-30 10:44:02 +0800

    优化代码

--- function/init_table.lua
+++ function/init_table.lua
@@ -1,7 +1,8 @@
 function table.Size( data )
-	local size = 0
-	for k , v in pairs( data ) do if k and v then size = size + 1 end end
-	return size
+	-- local size = 0
+	-- for k , v in pairs( data ) do if k and v then size = size + 1 end end
+	-- return size
+	return table_size( data )
 end
 
 function table.HasKey( data , key )
@@ -17,4 +18,10 @@ end
 function table.GetWithName( data , name )
 	for k , v in pairs( data ) do if v.name == name then return v end end
 	return nil
+end
+
+function table.ShallowCopy( data )
+	local new_data = {}
+	for k , v in pairs( data ) do new_data[k] = v end
+	return new_data
 end
