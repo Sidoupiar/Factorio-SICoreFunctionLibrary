f3772f68df812e573e25590beff7ea7c4cca9000
Sidoupiar <sidoupiar@qq.com>
2021-05-18 11:44:26 +0800

    调整逻辑代码

--- startup/generator/sigen_subgroup.lua
+++ startup/generator/sigen_subgroup.lua
@@ -1,4 +1,10 @@
 local entity = SIGen.Base:Copy( "subgroup" )
 entity:AddDefaultValue( "defaultType" , SITypes.subgroup )
 
+
+
+function entity:SetOrder( orderCode )
+	return self:SetParam( "order" , "z-"..SIGen.Order( orderCode ) )
+end
+
 return entity
