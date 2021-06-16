0d78d74882c47c95176d5305451d76382cd4d8ce
Sidoupiar <sidoupiar@qq.com>
2021-06-16 09:51:42 +0800

    优化代码

--- define/siflags.lua
+++ define/siflags.lua
@@ -10,28 +10,34 @@ SIFlags =
 		zk = "zkable" ,
 		zw = "zwable" ,
 		
-		hidden = "hidden" ,
-		noGapFill = "no-gap-fill-while-building" ,
-		notOnMap = "not-on-map" ,
-		notFlammable = "not-flammable" ,
-		notRotatable = "not-rotatable" ,
-		notRepairable = "not-repairable" ,
-		notBlueprintable = "not-blueprintable" ,
-		notDeconstructable = "not-deconstructable" ,
-		placeableEnemy = "placeable-enemy" ,
-		placeablePlayer = "placeable-player" ,
-		placeableNeutral = "placeable-neutral" ,
-		placeableOffGrid = "placeable-off-grid" ,
-		hideAltInfo = "hide-alt-info" ,
-		hideFromBonus = "hide-from-bonus-gui" ,
+		hidden               = "hidden" ,
+		noGapFill            = "no-gap-fill-while-building" ,
+		notOnMap             = "not-on-map" ,
+		notFlammable         = "not-flammable" ,
+		notRotatable         = "not-rotatable" ,
+		notRepairable        = "not-repairable" ,
+		notBlueprintable     = "not-blueprintable" ,
+		notDeconstructable   = "not-deconstructable" ,
+		placeableEnemy       = "placeable-enemy" ,
+		placeablePlayer      = "placeable-player" ,
+		placeableNeutral     = "placeable-neutral" ,
+		placeableOffGrid     = "placeable-off-grid" ,
+		hideAltInfo          = "hide-alt-info" ,
+		hideFromBonus        = "hide-from-bonus-gui" ,
 		fastReplaceableBuild = "fast-replaceable-no-build-while-moving" ,
 		fastReplaceableCross = "fast-replaceable-no-cross-type-while-moving" ,
-		building8Way = "building-direction-8-way" ,
-		breathsAir = "breaths-air" ,
-		playerCreation = "player-creation" ,
-		filterDirections = "filter-directions"
+		building8Way         = "building-direction-8-way" ,
+		breathsAir           = "breaths-air" ,
+		playerCreation       = "player-creation" ,
+		filterDirections     = "filter-directions"
 	} ,
-	directions = { "north" , "east" , "south" , "west" } ,
+	directions =
+	{
+		north = "north" ,
+		east  = "east" ,
+		south = "south" ,
+		west  = "west"
+	} ,
 	collisionMask =
 	{
 		ground        = "ground-tile" ,
@@ -95,23 +101,29 @@ SIFlags =
 		collidingWithTilesOnly  = "colliding-with-tiles-only"
 		
 	} ,
-	sciencePack =
-	{
-		key   = "description.science-pack-remaining-amount-key" ,
-		value = "description.science-pack-remaining-amount-value"
-	} ,
-	productType =
-	{
-		material = "material" ,
-		resource = "resource" ,
-		unit     = "unit"
-	} ,
 	trackType =
 	{
 		earlyGame = "early-game" , -- 载入游戏
 		menuTrack = "menu-track" , -- 主菜单
 		mainTrack = "main-track" , -- 游戏阶段
 		interlude = "interlude"    -- 插曲
+	} ,
+	
+	
+	
+	sciencePack =
+	{
+		key   = "description.science-pack-remaining-amount-key" ,
+		value = "description.science-pack-remaining-amount-value"
+	} ,
+	
+	
+	
+	productType =
+	{
+		material = "material" ,
+		resource = "resource" ,
+		unit     = "unit"
 	} ,
 	condition =
 	{
